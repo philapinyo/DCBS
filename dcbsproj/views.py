bde4475c52c58228bb9f593607c0f48b4a262705
philapinyo <stormjanes@gmail.com>
2024-01-05 19:17:05 +0000

    css

--- dcbsproj/views.py
+++ dcbsproj/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'dcbsproj/home.html')
+    return render(request, 'dcbsproj/home.html', {'title':'Welcome'})
 
 def about(request):
-    return render(request, 'dcbsproj/about.html')
+    return render(request, 'dcbsproj/about.html', {'title':'Welcome'})
 
 def contact(request):
-    return render(request, 'dcbsproj/contact.html')
+    return render(request, 'dcbsproj/contact.html', {'title':'Welcome'})
 
 
 # Create your views here.
